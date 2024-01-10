377f69831c8a3b8b71e87cc4997f66c51cc6a288
paulharrish <prasannaharrish@gmail.com>
2024-01-10 19:10:22 +0530

    Updated.

--- src/main/java/com/project/eventManagement/config/SecurityConfiguration.java
+++ src/main/java/com/project/eventManagement/config/SecurityConfiguration.java
@@ -53,6 +53,7 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/auth/**").permitAll();
+                    auth.requestMatchers("/events/all").permitAll();
                     auth.requestMatchers("/events/category/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
